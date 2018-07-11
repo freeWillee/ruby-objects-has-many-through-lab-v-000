fa365973be8068aaa9fa90aca2448c92c6064bd2
Will Lee <will.i.lee@outlook.com>
2018-07-11 20:57:19 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -16,7 +16,7 @@ class Patient
   end
 
   def appointments
-    Appointments.all.select do |appointment|
+    Appointment.all.select do |appointment|
       appointment.patient == self
     end
   end
