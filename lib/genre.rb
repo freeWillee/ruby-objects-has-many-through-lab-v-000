938438f4535b776c57ad3cc1c05b64a885d47d66
Will Lee <will.i.lee@outlook.com>
2018-07-11 20:06:32 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,6 +1,6 @@
 class Genre
   @@all = []
-  attr_accessor :name
+  attr_accessor :name, :songs
 
   def self.all
     @@all
@@ -9,5 +9,22 @@ class Genre
   def initialize(name)
     @name = name
     @@all < self
+    @songs = []
+  end
+
+  def new_song(name, artist)
+    self.songs << Song.new(name, artist, self)
+  end
+
+  def songs
+    Songs.all.select do |song|
+      song.genre == self
+    end
+  end
+
+  def artists
+    songs.select do |song|
+      songs.artist
+    end
   end
 end
